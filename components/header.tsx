0820af2b7dc2853352f29e65048d3f59f72092d5
Kesiena Berezi <berezikesiena@gmail.com>
2026-02-08 18:10:45 -0600

    commit

--- components/header.tsx
+++ components/header.tsx
@@ -35,10 +35,7 @@ export function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 md:h-20">
           
-          <Link
-            href="/"
-            className="-ml-4 sm:-ml-6 lg:-ml-8 pl-2 sm:pl-3 flex items-center gap-2"
-          >
+          <Link href="/"  className="-ml-4 sm:-ml-6 lg:-ml-8 pl-2 sm:pl-3 flex items-center gap-2">
             <Image
               src={getAssetPath("/PublicAffairsConference-2.png")}
               alt="Principia College"
