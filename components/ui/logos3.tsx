3a9dfbd4d698682e788344b15fc5cf95b12386d1
Kesiena Berezi <berezikesiena@gmail.com>
2026-02-08 18:59:22 -0600

    commit

--- components/ui/logos3.tsx
+++ components/ui/logos3.tsx
@@ -30,25 +30,25 @@ const Logos3 = ({
       description: "80 Years of thoughtful conversation",
       image: getAssetPath("/1.png"),
     
-      className: "h-30 w-auto",
+      className: "h-20 w-auto",
     },
     {
       id: "logo-2",
       description: "250+ Students",
       image: getAssetPath("/2.png"),
-      className: "h-30 w-auto",
+      className: "h-20 w-auto",
     },
     {
       id: "logo-3",
       description: "5000+ Attendees",
       image: getAssetPath("/3.png"),
-      className: "h-30 w-auto",
+      className: "h-20 w-auto",
     },
     {
       id: "logo-4",
       description: "100% Student-Led",
       image: getAssetPath("/4.png"),
-      className: "h-30 w-auto",
+      className: "h-20 w-auto",
     },
     
   ],
